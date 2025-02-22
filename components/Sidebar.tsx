d4e83ee04653062ebac6c13446a6de638da92822
Gurjant Singh <raiboyofficel@gmail.com>
2025-02-22 18:16:40 +0530

    Day2

---
+++ components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/16/solid";
+
+function Sidebar() {
+    return (
+        <div>
+           <div>
+            <DocumentMagnifyingGlassIcon className="h-16 md:w-16"
+           </div>   
+
+        <ul>
+
+        </ul>
+
+        </div>
+    )
+}
+
+export default Sidebar;
